819e49888098fa1309470a0947b64d9b0c7f56d1
Ujjwal <gifah18076@reebsd.com>
2024-12-02 18:25:36 +0530

    Selguide commit succesfully

---
+++ src/main/java/generic/OAuthEmailUtility.java
@@ -0,0 +1,16 @@
+//package generic;
+//
+//import java.net.MalformedURLException;
+//import java.util.Collections;
+//import java.util.concurrent.CompletableFuture;
+//import java.util.concurrent.ExecutionException;
+//
+//import com.microsoft.aad.msal4j.ClientCredentialFactory;
+//import com.microsoft.aad.msal4j.ClientCredentialParameters;
+//import com.microsoft.aad.msal4j.ConfidentialClientApplication;
+//import com.microsoft.aad.msal4j.IAuthenticationResult;
+//import com.microsoft.aad.msal4j.IConfidentialClientApplication;
+//
+//public class OAuthEmailUtility {
+//	
+//}
